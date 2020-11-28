adbf56c95b20d415d18f72d5aa9c4c182c68f81f
hashi1d <66625691+hashi1d@users.noreply.github.com>
2020-11-28 22:28:02 +0530

    Add files via upload

---
+++ pdfreadersecod.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<form method = "POST" enctype= "mulipart/form-data" action = "upload.php">
+<input type = "file" name ="file">
+<input type = "submit" value = "upload">
+</form>
+
+<?php
+
+// creating a file to uplaod
+//display all upld files
+
+
+$files = scandir("books for the web");
+for ($a = 2; $a <count ($files); $a++){
+//dizply link to download
+//mkrking it downodable
+?>
+<p>
+<a href = " books for the web/<?php echo $files[$a] ?>"><?php echo $files[$a] ?></a>
+</p>
+<?php
+}
