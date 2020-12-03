deb7c4c2216c3ce723153b8bd618dad1ef9e1cd0
hashi1d <66625691+hashi1d@users.noreply.github.com>
2020-12-03 22:55:21 +0530

    Add files via upload

---
+++ envi.php
@@ -0,0 +1,55 @@
+
+
+<?php
+  include "connection.php";
+  include "topnav.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+        <title> Book List</title>
+
+        <link rel="stylesheet" type="text/css" href="styleforbookimp.css">
+    <meta charset="utf-8">
+    </head>
+ <body>
+            <div class= "container">
+                <h1> Environment</h1>
+                 <div class = "book">
+                    <div class = "icon">
+                        <img src = "images/cat/tiger.jpg"  alt= ""  />
+                </div>
+                <div class = "content">
+                <h2> The Tiger: A True Story of Vengeance and Survival</h2>
+                <p> <I>  A gripping story of man pitted against nature's most fearsome and efficient predator. Outside a remote village in Russia's Far East a man-eating tiger is on the prowl. The tiger isn't just killing people, it's murdering them, almost as if it has a vendetta.</I> </p>
+                <p> Details about the book: <br>  Author: John Wiliyan </p>
+                <span> More text about the book: </span>
+                <a href = "books for the web/The Tiger_ A True Story of Vengeance and Survival ( PDFDrive ).pdf">
+                <button> Read it</button>
+                </a>
+                </div>
+</div>
+
+
+            <div class = "book">
+                    <div class = "icon">
+                        <img src = "images/cat/braindin.jpg"  alt= ""  />
+                    </div>
+            <div class = "content">
+                <h2>Braiding Sweetgrass: Indigenous Wisdom, Scientific Knowledge and the Teachings of Plants</h2>
+                <p> <i>Braiding Sweetgrass: Indigenous Wisdom, Scientific Knowledge, and the Teachings of Plants is a 2013 nonfiction book written by Indigenous author Robin Wall Kimmerer and published by Milkweed. The book is about the world of botany as described and explored through Native American traditions.</i> </p>
+                <p> Details about the book:  <br>   Author: Robin Wall</p>
+                <span> More text about the book: </span>
+                <a href = "books for the web/Braiding Sweetgrass_ Indigenous Wisdom, Scientific Knowledge and the Teachings of Plants ( PDFDrive ).pdf">
+                <button> Read it</button>
+                </a>
+            </div>
+</div>
+
+               
+               
+</div>
+</div>
+ </body>
+</html>
