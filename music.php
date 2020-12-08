b9e214cde04519578bf31549b688479a75e71cc6
hashi1d <66625691+hashi1d@users.noreply.github.com>
2020-12-09 00:10:53 +0530

    Add files via upload

---
+++ music.php
@@ -0,0 +1,41 @@
+
+
+<?php
+  include "connection.php";
+  include "topnav.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+        <title> Book List</title>
+
+        <link rel="stylesheet" type="text/css" href="styleforbookimp.css">
+    <meta charset="utf-8">
+    </head>
+ <body>
+            <div class= "container">
+                <h1>MUSIC</h1>
+                 <div class = "book">
+                    <div class = "icon">
+                        <img src = "images/cat/music1.jpg"  alt= ""  />
+                </div>
+                <div class = "content">
+                <h2> Guitar Scales Easy Music Lessons</h2>
+                <p> <I> The minor pentatonic scale is used by loads of guitarists, so it's a great first scale to learn. Although it has a long name, it's easy to play. It has five different notes, with the first note repeated at a higher pitch, so you play six notes in total. The first note is where the scale gets its name.</I> </p>
+                <p> Details about the book: <br>  English version</p>
+                <span> More text about the book: </span>
+                <a href = "books for the web/Guitar Scales Easy Music Lessons ( PDFDrive ).pdf">
+                <button> Read it</button>
+                </a>
+                </div>
+</div>
+
+
+            
+               
+               
+</div>
+</div>
+ </body>
+</html>
---
+++ painting.php
@@ -0,0 +1,41 @@
+
+
+<?php
+  include "connection.php";
+  include "topnav.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+        <title> Book List</title>
+
+        <link rel="stylesheet" type="text/css" href="styleforbookimp.css">
+    <meta charset="utf-8">
+    </head>
+ <body>
+            <div class= "container">
+                <h1>PAINTING & DRAWING</h1>
+                 <div class = "book">
+                    <div class = "icon">
+                        <img src = "images/cat/paint1.jpg"  alt= ""  />
+                </div>
+                <div class = "content">
+                <h2> Fun With A Pencil by Andrew Loomis</h2>
+                <p> <I> His first book, Fun With a Pencil, published in 1939 is a wonderfully crafted and engaging introduction to drawing, cartooning, and capturing the essence of a subject all while having fun. With delightful step-by-step instruction from Professor Blook, Loomis's charming alter ego on the page.</I> </p>
+                <p> Details about the book: <br>  Author: Alex Hayer</p>
+                <span> More text about the book: </span>
+                <a href = "books for the web/Andrew Loomis - Fun WIth a Pencil.pdf">
+                <button> Read it</button>
+                </a>
+                </div>
+</div>
+
+
+            
+               
+               
+</div>
+</div>
+ </body>
+</html>
